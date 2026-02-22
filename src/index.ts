30ff6d11d52e4655cc30323d4013be908dccb60f
Pedro Mesquita <pedrolucazxmesquita@gmail.com>
2026-02-22 15:09:16 -0300

    fix: resolve ESLint warnings and fix package.json syntax

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,7 @@ fastify.get("/", async () => {
 const start = async () => {
   try {
     await fastify.listen({ port: 3000 });
-    console.log("Server is running on http://localhost:3000");
+    fastify.log.info("Server is running on http://localhost:3000");
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
